409dd88919a6d43d66a27481743457459a5dc336
Zach LaCelle <zlacelle@mitre.org>
2019-03-07 11:24:28 -0500

    Initial adding of files.

---
+++ include/kvh_geo_fog_3d_driver.hpp
@@ -0,0 +1,25 @@
+/**
+ * @file driver.hpp
+ * @brief KVH Geo Fog 3D driver class header.
+ *
+ * This is the header file for the KVH Geo Fog 3D driver, responsible
+ * for interfacing to the unit over the serial connection.
+ */
+#pragma once
+
+namespace kvh
+{
+  /**
+   * @class Driver
+   * @ingroup kvh
+   * @brief Driver worker class for the KVH Geo Fog 3D.
+   */
+  class Driver
+  {
+  public:
+    Driver();
+    
+  private:
+    int placeholder_;
+  }; //end: class Driver  
+} //end: namespace kvh
---
+++ src/kvh_geo_fog_3d_driver.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file kvh_geo_fog_3d_driver.cpp
+ * @brief KVH Geo Fog 3D driver class definitions.
+ *
+ * This is the class definitions file for the KVH Geo Fog 3D driver,
+ * responsible for interfacing to the unit over the serial connection.
+ */
+#include "kvh_geo_fog_3d_driver.hpp"
+
+kvh::Driver::Driver()
+{
+
+} //end: Driver()
