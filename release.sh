4b8fe621ca94cd302ce21dc5f94a37161a51c677
Zach LaCelle <zlacelle@mitre.org>
2019-08-13 17:14:18 -0400

    Moving and fixing the release script.

--- release.sh
+++ release.sh
@@ -20,17 +20,15 @@ if [[ "$response" =~ ^(yes|y)$ ]]; then
    FIRST_RELEASE=1
 fi
 
-# Get into the base directory of the package, assuming this script is located
-# one directory deep
+# Get into the base directory of the repository
 pushd "${0%/*}" >& /dev/null
-pushd ..
 
 export ROSDISTRO_INDEX_URL=https://gitlab.mitre.org/dart-release/rosdistro/raw/master/index-v4.yaml
 export PYTHONHTTPSVERIFY=0
 if [ ${FIRST_RELEASE} -eq 0 ]; then
-    bloom-release --rosdistro kinetic --track kinetic kvh_geo_fog_3d_driver --no-web
+    bloom-release --rosdistro kinetic --track kinetic kvh_geo_fog_3d --no-web
 else
-    bloom-release --rosdistro kinetic --track kinetic kvh_geo_fog_3d_driver --no-web --edit
+    bloom-release --rosdistro kinetic --track kinetic kvh_geo_fog_3d --no-web --edit
 fi
 
 # Get back to normal
