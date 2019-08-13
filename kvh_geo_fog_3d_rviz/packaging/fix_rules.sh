f395734f4875fb72addcd29a31234c5dd0fe6efd
Zach LaCelle <zlacelle@mitre.org>
2019-08-13 16:27:04 -0400

    Fixing the fix_rules script.

--- kvh_geo_fog_3d_rviz/packaging/fix_rules.sh
+++ kvh_geo_fog_3d_rviz/packaging/fix_rules.sh
@@ -10,8 +10,24 @@ fi
 # This string tells our script that we've already fixed this file
 FLAG_STRING="# FIXED BY MITRE BUILD SCRIPTS"
 
+##
 # The lines containing local .cmake files
-CMAKE_CUSTOM_PACKAGES_LINES=\\t\\t-Dkvh_geo_fog_3d_msgs_DIR=\"/home/zlacelle/dart_workspace/catkin_ws/devel/share/kvh_geo_fog_3d_msgs/cmake\"
+##
+
+#This finds the location of the catkin_ws directory, which we will use as the path for the devel/ space
+parentname="$(dirname "$(pwd)")"
+while [ "$(basename "${parentname}")" != "catkin_ws" ]; do
+    parentname="$(dirname "${parentname}")"
+    if [ "${parentname}" == "/" ]; then
+        echo "Couldn't find catkin_ws directory! Unfortunately, this script requires this naming convention."
+        exit 1
+    fi
+done
+
+# The actual lines to add
+CMAKE_CUSTOM_PACKAGES_LINES=\\t\\t-Dkvh_geo_fog_3d_msgs_DIR=\"${parentname}/devel/share/kvh_geo_fog_3d_msgs/cmake\"
+
+##
 
 # The last non-blank line of the file
 line=`awk '/./{line=$0} END{print line}' debian/rules`
