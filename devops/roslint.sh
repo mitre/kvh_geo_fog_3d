6443e309c50ccd1e01354596747fe846c2553f8a
Zach LaCelle <zlacelle@mitre.org>
2019-09-30 17:25:46 +0000

    Fixed up the roslint runner.

--- devops/roslint.sh
+++ devops/roslint.sh
@@ -1,22 +1,46 @@
 #!/bin/bash
 
 # CD to script directory
-cd "${0%/*}"
-cd ..
+pushd "${0%/*}/.."  >& /dev/null
 
-echo "Script assumes you've already build everything!"
+echo "Script assumes you've already build everything and"
+echo " have sourced your workspace's setup.bash!"
 
-ROSLINT_DIR=roslint_output
+ROSLINT_DIR=$(pwd)/roslint_output
 
 if [ ! -d ${ROSLINT_DIR} ]; then
     mkdir -p ${ROSLINT_DIR}
+else
+    rm ${ROSLINT_DIR}/*
 fi
 
 # Have to use no-deps, otherwise it tries to build dependent packages with the roslint requirement, and if
 # they don't have it (e.g. msgs package) they will fail.
 # This means that dependent packages (e.g. msgs) must already be built, however!
 
-catkin build kvh_geo_fog_3d_driver --no-deps --make-args roslint >& ${ROSLINT_DIR}/kvh_geo_fog_3d_driver.txt
-catkin build kvh_geo_fog_3d_rviz --no-deps --make-args roslint >& ${ROSLINT_DIR}/kvh_geo_fog_3d_rviz.txt
+# Get our sourced WS
+WORKSPACE_ROOT=""
+IFS=':'
+read -ra CATKIN_WSES <<< "${CMAKE_PREFIX_PATH}"
+IFS=' '
+for ws in ${CATKIN_WSES}
+do
+    if [[ ${ws} != *"/opt/ros/"* ]]; then
+        # Remove /devel from string to get root
+        WORKSPACE_ROOT=${ws//\/devel/}
+    fi
+done
+
+pushd ${WORKSPACE_ROOT}/build
+
+pushd kvh_geo_fog_3d_driver >& /dev/null
+make roslint >& ${ROSLINT_DIR}/kvh_geo_fog_3d_driver.txt
+popd >& /dev/null
+pushd kvh_geo_fog_3d_rviz >& /dev/null
+make roslint >& ${ROSLINT_DIR}/kvh_geo_fog_3d_rviz.txt
+popd >& /dev/null
+
+popd >& /dev/null
+popd >& /dev/null
 
 echo "Finished with Ros linter!"
