9a1d5037cb2b5fdb502d15c92a4ac9c757f6b002
Zach LaCelle <zlacelle@mitre.org>
2019-10-09 00:30:02 +0000

    Adding copyright file headers.

--- kvh_geo_fog_3d_driver/src/kvh_driver/driver_main.cpp
+++ kvh_geo_fog_3d_driver/src/kvh_driver/driver_main.cpp
@@ -1,3 +1,22 @@
+/*********************************************************************
+ * Software License Agreement (Apache 2.0)
+ * 
+ *  Copyright (c) 2019, The MITRE Corporation.
+ *  All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *********************************************************************/
+
 /**
  * @file kvh_geo_fog_3d_driver.cpp
  * @brief KVH Geo Fog 3D driver class definitions.
