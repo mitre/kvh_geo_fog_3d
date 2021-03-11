1a3ed2674868a7996cf2d33303c606c7b916f605
Zach LaCelle <zlacelle@mitre.org>
2021-03-11 16:46:18 +0000

    Updating statick call to use new syntax for workspace.

--- devops/statick.sh
+++ devops/statick.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-statick_ws catkin_ws/src/kvh_geo_fog_3d --output-directory statick_output --user-paths devops/statick_config
+statick -ws catkin_ws/src/kvh_geo_fog_3d --output-directory statick_output --user-paths devops/statick_config
